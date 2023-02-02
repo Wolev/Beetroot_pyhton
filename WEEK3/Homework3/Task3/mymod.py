4bf81b9972775c40c81de851a6aca5fb514a1abb
wolev <me@example.com>
2023-02-02 19:18:02 +0200

    Do the missing mymod module exercise in the modules lesson.

--- WEEK3/Homework3/Task3/mymod.py
+++ WEEK3/Homework3/Task3/mymod.py
@@ -12,10 +12,8 @@ def count_chars(file):
 	print(f"Number of characters: {len(data_read)}")
 
 def test(file):
-	pass
-"""
-test(name) function that calls both counting functions with a given input file­name. Such a filename generally might be passed-in, hard-coded, input with raw_input, or pulled from a command-line via the sys.argv list; for now, assume it’s a passed-in function argument.
-"""
+	count_lines(file)
+	count_chars(file)
 
-count_lines("book.txt")	
-count_chars("book.txt")
+
+test("book.txt")
