915ca6b3995619a474b1e86dcaa874be7b084e66
wolev <me@example.com>
2023-02-05 12:27:26 +0200

    Make the arg_rules.py work in all conditions. The program is finished.

--- WEEK5/homework2/arg_rules.py
+++ WEEK5/homework2/arg_rules.py
@@ -6,32 +6,40 @@
 # otherwise, return the result;
 
 
-
 def arg_rules(type_: type, max_length: int, contains: list):
     def decorator(function):
 
-        def wrapper(argument_type, argument_len, argument_contains):
+        def wrapper(string: str) -> str:
+            """
+            -> str is a type hint.
+            a type hint specifies the return type of the function in Python
+            helps the IDE provide better code analysis suggest auto-completion options
+            """
+            # if type is not string print statement and return False
+            if not isinstance(string, type_):
+                """
+                type_ is a parameter in the arg_rules decorator
+                type_ specifies the type of the argument that the decorated function is expecting
+                type_ is string in this case 
+                """
+                print(f"Expected {type_}, but got {type(string)}.")
+                return False
 
-            def type_str(argument):
-                return type(argument) == type(str)
+            # if length of string is bigger that max_length print statement and return False
+            if len(string) > max_length:
+                print(f"The length of {string} is bigger than {max_length}.")
+                return False
 
-            def lenght_15(argument):
-                return len(argument) <= 15
+            # if symbol not in string print statement and return False
+            for symbol in contains:
+                if symbol not in string:
+                    print(f"The {symbol} symbol is not found in {string}.")
+                    return False
 
-            def contain(argument):
-                return 
-
-            type_str(argument_type)
-            lenght_15(argument_len)
-            def argument
-
-
-
-
+            # if all checks passed return function(string)
+            return function(string)
         return wrapper
-
     return decorator
-
 
 
 @arg_rules(type_=str, max_length=15, contains=['05', '@'])
@@ -40,4 +48,7 @@ def create_slogan(name: str) -> str:
 
 
 assert create_slogan('johndoe05@gmail.com') is False
+print("The assert is False\n")
+
 assert create_slogan('S@SH05') == 'S@SH05 drinks pepsi in his brand new BMW!'
+print(create_slogan('S@SH05'))
