fd59518aad2c0dee9cfbc98c99ac005d3b17b4aa
wolev <me@example.com>
2023-03-28 19:29:35 +0300

    Use a simpler method to get the first and last 2 chars in a new string.

--- WEEK1/HOMEWORK3/task1_03.py
+++ WEEK1/HOMEWORK3/task1_03.py
@@ -1,11 +1,34 @@
 #Task1: String it
 
-# declare the original string
+# Write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
+# If the string length is less than 2, return instead of the empty string.
+# Declare the original string.
+
 original_string = input("Input the original string:")
 
 # declare the final string
-result_string = ''
+result_string = ""
 
+# declare the iterator
+iterator = 0
+
+# better way
+
+while iterator <= len(original_string):
+	if len(original_string) == 1:
+		print("Empty String")
+		break
+	if len(original_string) == 2:
+		print(original_string * 2)
+		break
+	elif iterator == len(original_string):
+		result_string = original_string[0:2] + original_string[-2:]
+		print(result_string)
+	iterator += 1
+
+# old way that is too long
+
+"""
 # declare the iterator
 i = 0
 
@@ -29,3 +52,5 @@ while i <= len(original_string):
 	else:
 		pass
 	i+=1
+"""
+
